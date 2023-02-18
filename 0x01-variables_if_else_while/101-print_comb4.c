a02dba53021cef2fb9d624c3d42513b04a4930a1
Blaqdiamondz <krhoades226@gmail.com>
2023-02-17 21:17:21 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+/**
+ * main - Function
+ * Return: result
+ */
+
+int main(void)
+{
+
+	int hund = 0;
+
+	int tens = 1;
+	int unit = 2;
+
+	while (hund < 8)
+	{
+		int i = tens;
+		int k = unit;
+
+		while (tens <= 8 && i <= 8)
+		{
+			int j = k;
+
+			while (unit <= 9 && j <= 9)
+			{
+				if (i != j)
+				{
+					putchar(hund + '0');
+					putchar(i + '0');
+					putchar(j + '0');
+					if (hund < 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+
+				j++;
+			}
+			i++;
+			k++;
+		}
+		unit++;
+		tens++;
+		hund++;
+	}
+	putchar('\n');
+	return (0);
+}
+
